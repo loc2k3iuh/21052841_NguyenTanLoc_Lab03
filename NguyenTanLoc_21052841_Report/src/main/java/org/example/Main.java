f59d3b08f1ac9f3bde2b51f40e2c6939ccc96967
H82M19\Student <Student>
2025-03-01 08:47:30 +0700

    ThreadSafeSingleton

--- NguyenTanLoc_21052841_Report/src/main/java/org/example/Main.java
+++ NguyenTanLoc_21052841_Report/src/main/java/org/example/Main.java
@@ -3,28 +3,16 @@ package org.example;
 import org.example.singleton.EagerInitializedSingleton;
 import org.example.singleton.LazyInitializedSingleton;
 import org.example.singleton.StaticBlockSingleton;
+import org.example.singleton.ThreadSafeSingleton;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-
         Thread thread = new Thread(() -> {
-            LazyInitializedSingleton lazyInitializedSingleton = LazyInitializedSingleton.getInstance();
-            System.out.println(lazyInitializedSingleton.hashCode());
+            ThreadSafeSingleton threadSafeSingleton = ThreadSafeSingleton.getInstance();
+            System.out.println(threadSafeSingleton.hashCode());
         });
-
         thread.start();
-
-        Thread thread2 = new Thread(() -> {
-            LazyInitializedSingleton lazyInitializedSingleton = LazyInitializedSingleton.getInstance();
-            System.out.println(lazyInitializedSingleton.hashCode());
-        });
-
-        thread2.start();
-
-
-
     }
 }
